7e8ef201202277f42135aa5f0402d312960ec1fc
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2023-01-13 00:53:59 +0530

    Landing Section Added

--- client/src/components/Landing/landing.jsx
+++ client/src/components/Landing/landing.jsx
@@ -6,6 +6,12 @@ import Poverty from "../../media/poverty.jpg";
 export default function Landing(){
     return(
         <div className="landing-sec">
+        <div className="info-div">
+        <h1>CHANGE THE LIFE OF THOSE
+        <br></br>
+        WHO HAVE NO <b className="yellow">HOPE</b></h1>
+        <button className="donate-btn">Donate</button>
+        </div>
         <img src={Poverty} alt="Poverty" className="poverty-img"></img>
         </div>
     );
