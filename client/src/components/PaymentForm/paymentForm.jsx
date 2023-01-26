949ab3047cfcea82a763f6b7accdd5fd7da6542c
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2023-01-26 13:14:18 +0530

    Form Frontend

--- client/src/components/PaymentForm/paymentForm.jsx
+++ client/src/components/PaymentForm/paymentForm.jsx
@@ -4,7 +4,22 @@ import "./paymentForm.css";
 export default function PaymentForm(){
     return(
         <div className="payment-form">
-        
+        <div className="wrapper">
+        <form>
+            <input type="text" placeholder="Name"></input>
+            <input type="email" placeholder="Email"></input>
+            <label>Product: </label>
+            <input className="disable-cursor" type="text" placeholder="Product" readOnly={true}></input>
+            <label>Product ID: </label>
+            <input className="disable-cursor" type="text" placeholder="Product ID" readOnly={true}></input>
+            <label>Amount: </label>
+            <input className="disable-cursor" type="number" placeholder="Amount" readOnly={true}></input>
+            <button type="submit">Pay</button>
+        </form>
+        </div>
+        <div className="powered">
+            Powered by RECODEPAY
+        </div>
         </div>
     );
 }
